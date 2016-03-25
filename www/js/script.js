5801a1e65abec118b0e5cd722866b654556c109d
John Huiskes <ja.huiskes@student.avans.nl>
2016-03-25 13:22:15 +0100

    inserted loader

--- www/js/script.js
+++ www/js/script.js
@@ -1,6 +1,31 @@
+//retouneerd een async result, gebruik een promise in de code om deze te benaderen
+var getApiCall = function(url)
+{
+   return $.ajax({
+            url: url,
+            dataType: "json",
+            async: true,          
+        });        
+}
+
+$(document).ready(function(){
+console.log("ready");
+$('ul.pokelist').on("tap", 'li a' ,function() {
+    $.mobile.loading( 'show');
+  var promise = getApiCall($(this).data("link"))
+  promise.success(function (result) {
+                console.log(result);
+                    $.mobile.loading( 'hide');
+                    console.log("success");
+    }); 
+    console.log("clicked");
+});   
+
+});
+
 $(document).on('pageinit', '#details', function(){  
     console.log("reached details page");    
-    var url = "http://pokeapi.co/api/v2/pokemon/1";    
+  /*  var url = "http://pokeapi.co/api/v2/pokemon/1";    
      
     $.ajax({
         url: "http://pokeapi.co/api/v2/pokemon/" + 1,
@@ -13,42 +38,41 @@ $(document).on('pageinit', '#details', function(){
         error: function (request,error) {
             alert('Network error has occurred please try again!');
         }
-    });         
+    });   */      
 });
 
+            $(document).on('pageshow', '#pokedex', function(){        
+                $.mobile.loading( 'show');          
+            }); 
+
 $(document).on('pageinit', '#pokedex', function(){  
+       
     console.log("reached pokedex page");    
     var url = "http://pokeapi.co/api/v2/pokemon/?limit=251&offset=0";
    
-                      $('#pageLoader').show( "slow", function() { }); 
-                      $('#pageLoader').html('<img src="images/loader.gif" />');  
-                      console.log("waiting");   
-       
-     
-    $.ajax({
-        url: url,
-        dataType: "json",
-        async: true,
-        success: function (result) {
+   console.log("waiting");   
+                var promise = getApiCall(url);
+                promise.success(function (result) {
+                console.log(result);
+                    jQuery.each(result.results, function(index,value){
+                        console.log(value.name);
+                        fillPokemon(index,value);
+                    })
+                    console.log("success");
+                    $.mobile.loading( 'hide'); 
 
-        console.log(result);
-        jQuery.each(result.results, function(index,value){
-            console.log(value.name);
-            fillPokemon(index,value);
-        })
-        console.log("success");
-        },
-        error: function (request,error) {
-            alert('Network error has occurred please try again!');
-        }
-    });         
+                });
+                promise.error(function (request,error) {
+                        alert('Network error has occurred please try again!');
+                    }); 
+  
 });
 
  
 var fillPokemon = function(index,value){
      $("#pokelistdata").append(
                     "<li>" +
-                    "<a href ='#details'>" +
+                    "<a class='pokedetail' data-link ='"+ value.url + "'>" +
                     "<img src='img/pokemon/" + (index + 1) + ".png'>"+
                     "<h1>" + value.name + "</h1>" +
                     "</a>" +
